c08dc43726887a9393043bac8ed3d0c0054addf3
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-10-13 10:23:09 -0300

    creando modelos ts

---
+++ Frontend/usuario-frontend/src/app/actividad-usuario/models/ActividadesUsuarioModel.ts
@@ -0,0 +1,6 @@
+export class ActividadesUsuarioModel {
+  id_actividad: number = 0;
+  create_date: string = '';
+  id_usuario: number = 0;
+  actividad: string = '';
+}
---
+++ Frontend/usuario-frontend/src/app/actividad-usuario/models/CredencialesUsuarioModel.ts
@@ -0,0 +1,10 @@
+export class CredencialesUsuarioModel {
+  id: number = 0;
+  nombre: string = '';
+  apellido: string = '';
+  correoElectronico: string = '';
+  fechaNacimiento: string = '';
+  telefono: number = 0;
+  paisResidencia: string = '';
+  informacionContacto: boolean = false;
+}
