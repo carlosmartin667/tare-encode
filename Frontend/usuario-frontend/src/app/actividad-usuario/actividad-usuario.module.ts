c90c121b84d9ab843fa40185e5f2333d14c533a7
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-10-15 00:36:29 -0300

    creando pagina de editar

--- Frontend/usuario-frontend/src/app/actividad-usuario/actividad-usuario.module.ts
+++ Frontend/usuario-frontend/src/app/actividad-usuario/actividad-usuario.module.ts
@@ -6,9 +6,10 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { UsuarioServicio } from './services/usuario.service';
 import { ListaActividadComponent } from './lista-actividad/lista-actividad.component';
 import { FormsModule } from '@angular/forms';
+import { EditarComponent } from './editar/editar.component';
 
 @NgModule({
-  declarations: [ListaComponent, FormularioComponent, ListaActividadComponent],
+  declarations: [ListaComponent, FormularioComponent, ListaActividadComponent, EditarComponent],
   imports: [CommonModule, HttpClientModule, FormsModule],
   exports: [ListaComponent],
   providers: [UsuarioServicio],
--- Frontend/usuario-frontend/src/app/app-routing.module.ts
+++ Frontend/usuario-frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarComponent } from './actividad-usuario/editar/editar.component';
 import { FormularioComponent } from './actividad-usuario/formulario/formulario.component';
 import { ListaActividadComponent } from './actividad-usuario/lista-actividad/lista-actividad.component';
 import { ListaComponent } from './actividad-usuario/lista/lista.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'lista', component: ListaComponent },
   { path: '', component: FormularioComponent },
   { path: 'listaUsuario/:id', component: ListaActividadComponent },
+  { path: 'editar/:id', component: EditarComponent },
 ];
 
 @NgModule({
